ac7e4e20e2c4b9ee3fc9b675fd3bca6d495b884f
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2017-05-08 17:34:52 +0100

    lots of lots  of treesssssssssssssssssssssssss

---
+++ KojimaDrive/Assets/Chaos/Scripts/Tree.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour {
+
+
+    public GameObject tree = null;
+
+
+	// Use this for initialization
+	void Start () {
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit))
+        {
+            transform.position = hit.point;
+
+        }
+        
+	}
+	
+}
